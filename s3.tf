8afcbd85dd71b33d8a34014012b0317412c20117
Rahulk218 <gowdarahul1819@gmail.com>
2024-06-16 13:03:02 +0530

    updated s3.tf file

--- s3.tf
+++ s3.tf
@@ -1,6 +1,6 @@
 #create s3 bucket
 resource "aws_s3_bucket" "mybucket" {
-  bucket = var.bucketname
+  bucket = var.bucket_name
 }
 resource "aws_s3_bucket_ownership_controls" "example" {
   bucket = aws_s3_bucket.mybucket.id
