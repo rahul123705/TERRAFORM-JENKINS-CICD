74f16abd4e24e434554a6b4c6f9f831a99c9c794
Rahulk218 <gowdarahul1819@gmail.com>
2024-06-16 13:25:10 +0530

    fix

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,7 @@
-output "websiteendpoint" {
-    value = aws_s3_bucket.mybucket.website_domain
+output "website_endpoint" {
+  value = aws_s3_bucket_website_configuration.mybucket.website_endpoint
 }
+
 
 output "public_ip"{
     value = aws_instance.Rahul.public_ip
